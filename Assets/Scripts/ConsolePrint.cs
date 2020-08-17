9cd26e6310ab5f12342106b9b6d3c63d79afc3e0
Ash Zhang <zblash@outlook.com>
2020-08-17 14:15:20 +1000

    80 percent complete. print the number and name in the console

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.name + ":" + i);
+        i++;
+    }
+}
